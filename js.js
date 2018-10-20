65ec2060cad626d142956d4f9b7bf7fdc83fd01f
Krzysztof Kozłowski <kozlowski.k@o2.pl>
2018-10-21 00:28:32 +0200

    naprawiony XMLHttpRequest

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+const pages = document.getElementsByClassName('menu');
+Array.from(pages).forEach( el => el.addEventListener('click', changePage));
+
 const changePage = (ev) => {
   ev.preventDefault();
   const page = ev.target.textContent.toLowerCase();
@@ -10,6 +13,3 @@ const changePage = (ev) => {
   xhttp.open('GET', `https://czarny8501.github.io/pages/${page}.html`, true);
   xhttp.send();
 }
-
-const pages = document.getElementsByClassName('menu');
-Array.from(pages).forEach( el => el.addEventListener('click', changePage));a
