584360efdda41b34955680cdf37a2a6585df39e7
Krzysztof Kozłowski <kozlowski.k@o2.pl>
2018-10-20 22:47:08 +0200

    if statement corrected

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ const changePage = (ev) => {
   const page = ev.target.textContent.toLowerCase();
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = () => {
-    this.readyState == 4 && this.status == 200 ? console.log('response : ') : null;
+    if(this.readyState == 4 && this.status == 200) document.getElementById('content').innerHTML = this.responseText;
   };
   xhttp.open('GET', `pages/${page}.html`, true);
   xhttp.send();
