49e2129798c1a234f93a7f981042052037806cc7
Krzysztof Kozłowski <kozlowski.k@o2.pl>
2018-10-21 05:26:54 +0200

    toggle activ, poprawiony if

--- js.js
+++ js.js
@@ -1,15 +1,17 @@
+const pages = document.getElementsByClassName('menu');
+
 const changePage = (ev) => {
   ev.preventDefault();
+  if (!ev.target.classList.contains('active')) {
+    Array.from(pages).forEach(el => el.classList.contains('active') && el.classList.remove('active'));
+    ev.target.className += ' active';
+  }
   const page = ev.target.textContent.toLowerCase();
   const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = () => {
-    if(xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById('content').innerHTML = xhttp.responseText;
-    }
-  };
+  xhttp.onreadystatechange = () => xhttp.readyState == 4 && xhttp.status == 200 ? document.getElementById('content').innerHTML = xhttp.responseText : null;
+    
   xhttp.open('GET', `https://czarny8501.github.io/pages/${page}.html`, true);
   xhttp.send();
 }
 
-const pages = document.getElementsByClassName('menu');
-Array.from(pages).forEach( el => el.addEventListener('click', changePage));
+Array.from(pages).forEach(el => el.addEventListener('click', changePage));
